6984af3971828db2b8c436504e96dfbd7b7b1ac2
Irek <yairek@gmail.com>
2024-03-11 21:41:27 +0100

    Initial Commit

---
+++ Test_IunusovIrek/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Test_IunusovIrek
+//
+//  Created by Reek i on 11.03.2024.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Test_IunusovIrek/Test_IunusovIrekApp.swift
@@ -0,0 +1,17 @@
+//
+//  Test_IunusovIrekApp.swift
+//  Test_IunusovIrek
+//
+//  Created by Reek i on 11.03.2024.
+//
+
+import SwiftUI
+
+@main
+struct Test_IunusovIrekApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
